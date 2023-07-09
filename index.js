1e985f6084a24deb663fb5c5b51b983321768793
zerais <davesiarez18@gmail.com>
2023-07-09 18:43:11 +0800

    v2 music and conversation added

--- index.js
+++ index.js
@@ -3,9 +3,87 @@ var c = canvas.getContext('2d');
 canvas.width = window.innerWidth-7.9;
 canvas.height = window.innerHeight-7.9;  
 
-var textSpeed=1000;
+var textSpeed=882;
 var myInterval;
+var audio = new Audio('Zild - Medisina (Official Video).mp3');
+audio.currentTime = 200;
 
+var conversation = 
+[
+    "-Hi",
+    "",
+    "",
+    "",
+    "",
+    "-Ikaw nanaman haha",
+    "",
+    "",
+    "",
+    "",
+    "-Oo nga eh haha kumusta",
+    "",
+    "",
+    "",
+    "",
+    "-Eto busy sa kung ano ano haha",
+    "",
+    "",
+    "",
+    "",
+    "-Sorry kung nangungulit ako",
+    "",
+    "",
+    "",
+    "",
+    "-Ano bang meron?",
+    "",
+    "",
+    "",
+    "",
+    "-Hindi ka nawawala sa isip ko",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "-Hindi mo naman ako mahal, malungkot ka lang",
+    "",
+    "",
+    "",
+    "",
+    "-Bakit ganyan nanaman?",
+    "",
+    "",
+    "",
+    "",
+    "-Kinakawawa lang natin sarili natin",
+    "",
+    "",
+    "",
+    "",
+    "-Ayoko na",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "-Lagi mo naman yan sinasabi",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "-Paulit ulit lang naman tayo diba?",
+    "",
+    "",
+    "",
+    "",
+    "",
+]
+
+var conversationHeight = 20;
+let index=0;
 const Fonts = [
     "Arial",
     "Helvetica",
@@ -28,91 +106,143 @@ const Fonts = [
     "Rockwell",
     "Copperplate",
     "Abracadabra",
-  "Agony",
-  "Alien Encounter",
-  "Amnesia",
-  "Ancient Curse",
-  "Apparition",
-  "Asylum",
-  "Banshee",
-  "Bat Wings",
-  "Black Widow",
-  "Blood Lust",
-  "Boneshaker",
-  "Boogeyman",
-  "Cemetery Gates",
-  "Chainsaw Massacre",
-  "Cobwebbed",
-  "Corpse Bride",
-  "Creepshow",
-  "Crypt Keeper",
-  "Cursed Scroll",
-  "Dark Carnival",
-  "Dark Shadows",
-  "Death Drips",
-  "Demon Eyes",
-  "Despair",
-  "Dracula's Coffin",
-  "Eerie Whisper",
-  "Evil Clown",
-  "Fear Factory",
-  "Foggy Night",
-  "Frankenfont",
-  "Funeral March",
-  "Ghost Town",
-  "Ghoul Gaze",
-  "Gravestone",
-  "Grim Reaper",
-  "Haunted House",
-  "Hellfire",
-  "Hocus Pocus",
-  "Insidious",
-  "Jack-O-Lantern",
-  "Lurking Shadows",
-  "Mad Scientist",
-  "Midnight Howl",
-  "Moonlight Serenade",
-  "Mummy Wrap",
-  "Nightmare",
-  "Occult Ritual",
-  "Ominous Storm",
-  "Phantom Fingers",
+    "Agony",
+    "Alien Encounter",
+    "Amnesia",
+    "Ancient Curse",
+    "Apparition",
+    "Asylum",
+    "Banshee",
+    "Bat Wings",
+    "Black Widow",
+    "Blood Lust",
+    "Boneshaker",
+    "Boogeyman",
+    "Cemetery Gates",
+    "Chainsaw Massacre",
+    "Cobwebbed",
+    "Corpse Bride",
+    "Creepshow",
+    "Crypt Keeper",
+    "Cursed Scroll",
+    "Dark Carnival",
+    "Dark Shadows",
+    "Death Drips",
+    "Demon Eyes",
+    "Despair",
+    "Dracula's Coffin",
+    "Eerie Whisper",
+    "Evil Clown",
+    "Fear Factory",
+    "Foggy Night",
+    "Frankenfont",
+    "Funeral March",
+    "Ghost Town",
+    "Ghoul Gaze",
+    "Gravestone",
+    "Grim Reaper",
+    "Haunted House",
+    "Hellfire",
+    "Hocus Pocus",
+    "Insidious",
+    "Jack-O-Lantern",
+    "Lurking Shadows",
+    "Mad Scientist",
+    "Midnight Howl",
+    "Moonlight Serenade",
+    "Mummy Wrap",
+    "Nightmare",
+    "Occult Ritual",
+    "Ominous Storm",
+    "Phantom Fingers",
 ];
 
 function startInterval() {
     myInterval = setTimeout(printFont, textSpeed);
 }
 
-//c.fillText("VICTORY!", (canvas.width/2)-80, 50);
-function printFont(){
-    textSpeed -= 15;
-    console.log(textSpeed)
-
+function printText(){
     randFont = Fonts[Math.floor(Math.random() * 70)];
-
     c.beginPath();
-    c.font = "20px " + randFont;
+    c.font = "15px " + randFont;
     c.fillStyle = "#FFFFFF";
     c.fillText("Let Me Goooooooo!!!!!", Math.floor(Math.random() * canvas.width), Math.floor(Math.random() * canvas.height));
 
-    myInterval = setTimeout(printFont, textSpeed);
+}
 
-    if(textSpeed <= -40000){
-        clearInterval(myInterval);
-        c.clearRect(0, 0, canvas.width,canvas.height)
+
+function printFont(){
+    audio.play();
+    
+    console.log(textSpeed);
+    console.log(audio.currentTime);
+
+    
+    if(audio.currentTime > 212){
+
         c.beginPath();
-        c.font = "100px " + randFont;
+        c.font = "20px dasjk";
         c.fillStyle = "#FFFFFF";
-        c.fillText("Let Me Goooooooo!!!!", canvas.width/2 - 400, canvas.height/2);
-
+        c.fillText(conversation[index], 100, conversationHeight);
+        conversationHeight+=7;
+        index++;
     }
+   
+    if(audio.currentTime > 250){
+        printText();
+    }
+    if(audio.currentTime > 255){
+        textSpeed =220;
+    }
+    if(audio.currentTime > 260){
+        textSpeed =110;
+    }
+    if(audio.currentTime > 265){
+        textSpeed =0;
+    }
+    if(audio.currentTime >= 270){
+        textSpeed =110;
+        c.clearRect(0, 0, canvas.width,canvas.height)
+        c.font = "80px " + randFont;
+        c.fillStyle = "#FF0000";
+        c.fillText("I LOVE YOU MY MEDICINEEEEE!!!!", canvas.width/2 - 600, canvas.height/2);
+    }
+
+    if(audio.currentTime >= 277){
+        textSpeed =110;
+        c.clearRect(0, 0, canvas.width,canvas.height)
+        c.font = "80px " + randFont;
+        c.fillStyle = "#FF0000";
+        c.fillText("MAHAL KITA MY MEDICINE!!!!", canvas.width/2 - 600, canvas.height/2);
+    }
+    if(audio.currentTime >= 284){
+        textSpeed =110;
+        c.clearRect(0, 0, canvas.width,canvas.height)
+        c.font = "80px " + randFont;
+        c.fillStyle = "#FF0000";
+        c.fillText("I LOVE YOU MY MEDICINEEEEE!!!!", canvas.width/2 - 600, canvas.height/2);
+    }
+    if(audio.currentTime >= 292){
+        textSpeed =110;
+        c.clearRect(0, 0, canvas.width,canvas.height)
+        c.font = "80px " + randFont;
+        c.fillStyle = "#FF0000";
+        c.fillText("MAHAL KITA MY MEDICINE!!!!", canvas.width/2 - 600, canvas.height/2);
+    }
+    if(audio.currentTime >= 298){
+        c.clearRect(0, 0, canvas.width,canvas.height)
+        c.font = "80px " + randFont;
+        c.fillStyle = "#FF0000";
+        c.fillText("MADE BY: DAVE SIAREZ", canvas.width/2 - 600, canvas.height/2);
+    }
+    if(audio.currentTime >= 304){
+        c.clearRect(0, 0, canvas.width,canvas.height)
+    }
+
+    
+    myInterval = setTimeout(printFont, textSpeed);
+  
 }
 
 
 startInterval();
-
-
-
-
-
-
